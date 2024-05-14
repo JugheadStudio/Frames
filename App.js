a5e32271c4d7eb00aa62996c4493e6042ee881b0
Jugz <55399963+JugheadStudio@users.noreply.github.com>
2024-05-14 18:49:48 +0200

    init with firebase connected and navigation working

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import HomeScreen from './screens/HomeScreen';
+import DevelopmentScreen from './screens/DevelopmentScreen';
+import PhotoScreen from './screens/PhotosScreen';
+import PrivateScreen from './screens/PrivateScreen';
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home-outline' : 'home-outline';
+          } else if (route.name === 'Development') {
+            iconName = focused ? 'code-outline' : 'code-outline';
+          } else if (route.name === 'Photos') {
+            iconName = focused ? 'image-outline' : 'image-outline';
+          } else if (route.name === 'Private') {
+            iconName = focused ? 'accessibility-outline' : 'accessibility-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Development" component={DevelopmentScreen} />
+        <Tab.Screen name="Photos" component={PhotoScreen} />
+        <Tab.Screen name="Private" component={PrivateScreen} />
+        
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
